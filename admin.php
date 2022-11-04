afbfec7078f1c52434847b1d29054509ab06ed16
comibear <wonsang4232@gmail.com>
2022-11-04 12:30:34 +0900

    main

--- admin.php
+++ admin.php
@@ -1,3 +1,12 @@
 <?php
     echo "hello admin";
 ?>
+<html>
+<header>
+    <p>hello admin</p>
+</header>
+<body>
+    <a class="btn btn-success" href="logout.php" style="margin-left: 50px">
+    <span class="glyphicon glyphicon-user"></span>&nbsp;logout</a>
+</body>
+</html>
--- user.php
+++ user.php
@@ -1,10 +1,21 @@
 <?php
-    echo "Hello User";
+    include "dbconnect.php";
+    include "check.php";
+    if($chk == False)
+    {
+        header("Location: logout.php");
+    }
+    $admin = "SELECT is_admin FROM member WHERE id = '{$_SESSION['username']}' ";
+    $result = @mysqli_fetch_array(mysqli_query($db,$admin));
+    if($result['is_admin'] == 1)
+    {
+        header("Location: admin.php");
+    }
 ?>
 <html>
-<head>
+<header>
     <p>hello user</p>
-</head>
+</header>
 <body>
     <a class="btn btn-success" href="logout.php" style="margin-left: 50px">
     <span class="glyphicon glyphicon-user"></span>&nbsp;logout</a>
