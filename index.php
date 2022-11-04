9843604bfa7fd6ff7c65314cd1b8a950a74300ed
comibear <wonsang4232@gmail.com>
2022-11-04 10:48:41 +0900

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@
 		</div>
 		</br>
 		<div class="from-group" style="text-align:center">
-			<button type="submit" name="login" class="btn btn-success">Login</button>
+			<button type="submit" name="login" class="btn btn-success" value="login">Login</button>
 			<a class="btn btn-success" href="register.php" style="margin-left: 50px">
 			<span class="glyphicon glyphicon-user"></span>&nbsp;Register</a>
 		</div>
